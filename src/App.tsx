4bdd91d2e4759f3e01b6083ed20cc9f99f3c203f
Peter <hoffmannpeter82@gmail.com>
2023-02-03 12:35:26 +0100

    Reset Playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorComponent from "./components/ErrorComponent";
-import MainHeader from "./components/MainHeader";
-import Contact from "./components/pages/Contact";
-import ProductDetails from "./components/pages/ProductDetails";
-import Products from "./components/pages/Products";
-import Welcome from "./components/pages/Welcome";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainHeader />,
-    errorElement: <ErrorComponent />,
-    children: [
-      { path: "/welcome", element: <Welcome /> },
-      { path: "/products", element: <Products /> },
-      { path: "/products/:productId", element: <ProductDetails /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-]);
-
 const App: React.FC = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
