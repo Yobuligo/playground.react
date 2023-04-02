2124dee9e55f3360253cde34bfbfaf38b1f08a59
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-02 10:50:50 +0200

    Refactor app

--- src/components/context/AppContext.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { initializeContext } from "./InitialContext";
-
-export const AppContext = React.createContext(initializeContext());
--- src/components/context/IContext.ts
+++
@@ -1,6 +0,0 @@
-import { ITodo } from "../../model/ITodo";
-
-export interface IContext {
-  todos: ITodo[];
-  onToggleTodo: (todo: ITodo) => void;
-}
