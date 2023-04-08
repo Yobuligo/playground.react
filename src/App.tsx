07715bfd7aad06636c15ea5ff8e726a7675740eb
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-08 14:54:20 +0200

    Initial commit endless chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,5 @@
-import { ReactNode, useEffect, useState } from "react";
-import NewRow from "./components/NewRow";
-import { State } from "./model/State";
-import { AppContext } from "./store/AppContext";
-
 const App: React.FC = () => {
-  const x = 5;
-  const y = 5;
-  const [fields, setFields] = useState<State[][]>([]);
-  const [rowItems, setRowItems] = useState<ReactNode>();
-
-  useEffect(() => {
-    setFields(() => {
-      const rows: State[][] = [];
-      for (let i = 0; i < y; i++) {
-        const fields: State[] = [];
-        for (let j = 0; j < x; j++) {
-          fields.push(State.Empty);
-        }
-        rows.push(fields);
-      }
-      return rows;
-    });
-  }, [x, y]);
-
-  const [mode, setMode] = useState(false);
-  const toggleMode = () => {
-    setMode((previous) => !previous);
-  };
-
-  useEffect(() => {
-    setRowItems(() => {
-      if (fields.length === 0) {
-        return <></>;
-      }
-      let rows: ReactNode[] = [];
-      for (let i = 0; i < y; i++) {
-        const rowFields: State[] = [];
-        for (let j = 0; j < x; j++) {
-          rowFields.push(fields[j][i]);
-        }
-        rows.push(<NewRow states={rowFields} />);
-      }
-      return rows;
-    });
-  }, [fields]);
-
-  return (
-    <AppContext.Provider value={{ mode: mode, toggleMode: toggleMode }}>
-      {rowItems}
-    </AppContext.Provider>
-  );
+  return <></>;
 };
 
 export default App;
--- src/store/IAppContext.ts
+++
@@ -1,4 +0,0 @@
-export interface IAppContext {
-  mode: boolean;
-  toggleMode: () => void;
-}
