7827bfee98d3c19c6797a6ecae9a2c6b427ac17a
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-08 16:35:06 +0200

    Disable input when type writing

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const UserInput: React.FC<{
   onConfirm: (text: string) => void;
   typeWriterActive: boolean;
 }> = (props) => {
   const [enteredText, setEnteredText] = useState("");
+  const input = useRef<HTMLInputElement>(null);
+  const [disabled, setDisabled] = useState(false);
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     props.onConfirm(enteredText);
@@ -13,19 +15,24 @@ const UserInput: React.FC<{
   useEffect(() => {
     if (props.typeWriterActive) {
       setEnteredText("");
+      setDisabled(true);
+    } else {
+      input.current?.focus();
+      setDisabled(false);
     }
   }, [props.typeWriterActive]);
 
   return (
     <>
       <form action="" onSubmit={onSubmit}>
-        <label htmlFor="userInput">Enter the chain</label>
         <input
           id="userInput"
           name="userInput"
           type="text"
           value={enteredText}
           onChange={(event) => setEnteredText(event.target.value)}
+          disabled={disabled}
+          ref={input}
         />
         <button>Confirm</button>
       </form>
